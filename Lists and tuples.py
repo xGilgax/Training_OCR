0eab6ed5d19278ad1691cf31eaaac7ad2c480805
xGilgax <jjmatthieu@hotmail.fr>
2020-09-16 13:40:49 +0200

    List and tuples unknown function parameters

--- Lists and tuples.py
+++ Lists and tuples.py
@@ -38,3 +38,40 @@ def eucl_div(diviseur, dividande):
 ent_part, scraches = eucl_div(53, 8)
 
 print('Partie entière {}\nReste {}'.format(ent_part, scraches))
+
+
+chain = "Hello my name is Stein"
+list_of_string = ['My', 'name', 'is', 'Stein']
+
+split_chain = chain.split()
+print('split chain:  {}'.format(split_chain))
+
+list_to_chain = " ".join(list_of_string)
+print(("List to chain: {}".format(list_to_chain)))
+
+def display_float(number):
+    try:
+        number = float(number)
+    except ValueError:
+        print('Not a number')
+    split_number = str(number).split('.')
+
+    return ','.join([split_number[0], split_number[1][:3]])
+
+print(display_float(9.7896))
+
+
+def afficher(*parameters, sep=' ', end='\n'):
+
+    parameters = list(parameters)
+    for i, parameter in enumerate(parameters):
+        parameters[i] = str(parameter)
+    chain_to_display = sep.join(parameters)
+    chain_to_display += end
+    print(chain_to_display)
+
+    return chain_to_display
+
+chain_to_display = afficher(first_list, first_tuple)
+
+print(chain_to_display)
