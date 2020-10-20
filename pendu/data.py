b4d72a1656f855479a84b777d02a8fe0b896fe16
xGilgax <jjmatthieu@hotmail.fr>
2020-10-20 23:08:46 +0200

    Pendu on avance sur les fonctions 2

--- pendu/data.py
+++ pendu/data.py
@@ -1,1 +1,3 @@
 path_to_score = 'C:\Users\Cobra\PycharmProjects\Training_OCR\pendu'
+scores_file_name = 'scores'
+word_list = ()
--- pendu/functions.py
+++ pendu/functions.py
@@ -50,15 +50,43 @@ def check_scores_file(path_to_score_folder, scores_file_name):
     return None
 
 
-def set_player_score():
+def set_player_score(player_name, score):
+    """Modify the score of a player (score is the score actualised)"""
     return None
 
 
-player_name = set_player_name()
-print("Your name is: {}".format(player_name))
+def points_earned(word_lenght, tries_number):
+    points = None
+    return points
+
+
+def pick_random_word(word_list):
+    return None
+
+
+def generate_hidden_word(word):
+    hidden_word = None
+    return hidden_word
+
+
+def actualise_hidden_word(word):
+    actualised_word = None
+    return actualised_word
+
+
+def check_win(hidden_word, word_to_find):
+    win_status = None #Bool
+    return win_status
+
+
+
 
 path_to_score = 'C:/Users/Cobra/PycharmProjects/Training_OCR/pendu'
 scores_file_name = 'scores'
+word_list = {}
+
+player_name = set_player_name()
+print("Your name is: {}".format(player_name))
 
 player_score = set_player(player_name, path_to_score, scores_file_name)
 
